c2d3f70dba2345bdaa89a4353a22115c43162d6f
Alex P from nllab_ot2 <a.j.p.perkins@sms.ed.ac.uk>
2022-10-08 18:31:46 +0100

    changed dilutions and loads of heights and aspiration heights as appropriate

--- bradford_plating_ot2_script.py
+++ bradford_plating_ot2_script.py
@@ -78,10 +78,10 @@ def run(protocol: protocol_api.ProtocolContext):
             right_pipette.touch_tip()
 
             # Still dispensing 1mm above the bottom
-            right_pipette.dispense(dispense_volume, well.top(3), rate=0.2)
+            right_pipette.dispense(dispense_volume, well.top(3), rate=0.5)
             protocol.delay(seconds=2)
-
-            right_pipette.blow_out()
+            right_pipette.move_to(well.top())
+            protocol.delay(seconds=2)
 
 
     def distribute_protein_mix(aspirate_well, aspirate_plate, dispense_well, dispense_volume):
@@ -96,7 +96,30 @@ def run(protocol: protocol_api.ProtocolContext):
             # plus 10%
             aspirate_volume = dispense_volume + (dispense_volume*0.1)
 
-            left_pipette.aspirate(aspirate_volume, aspirate_plate[aspirate_well].bottom(4), rate=0.5)
+            left_pipette.aspirate(aspirate_volume, aspirate_plate[aspirate_well].bottom(2), rate=0.5)
+            protocol.delay(seconds=2)
+            left_pipette.touch_tip()
+
+            # Still dispensing 1mm above the bottom
+            left_pipette.dispense(dispense_volume, dispense_well.top(-4), rate=0.3)
+            protocol.delay(seconds=1)
+            left_pipette.touch_tip()
+
+            left_pipette.drop_tip()
+
+    def distribute_standards_mix(aspirate_well, aspirate_plate, dispense_well, dispense_volume):
+
+        if dispense_volume == 0:
+            pass
+
+        else:
+
+            left_pipette.pick_up_tip()
+
+            # plus 10%
+            aspirate_volume = dispense_volume + (dispense_volume*0.1)
+
+            left_pipette.aspirate(aspirate_volume, aspirate_plate[aspirate_well].bottom(1), rate=0.5)
             protocol.delay(seconds=2)
             left_pipette.touch_tip()
 
@@ -113,191 +136,221 @@ def run(protocol: protocol_api.ProtocolContext):
 
     # Dilutions
 
-    ##################################### Dispense stock 30
+    dilution_toggle = True
+    plate_sample_dilutions = True
+    bradford_toggle = True
+    plate_standards_toggle = False
 
-    stock_30 = np.array([[200, 120, 200, 150], [120, 150, 100, 270], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    if dilution_toggle:
+        ##################################### Dispense stock 30
 
-    # flatten array
-    stock_30_vols = stock_30.reshape(-1)
+        stock_30 = np.array([[80, 50, 80, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
 
-    stock_30_aspirate_height = 88.5
+        # flatten array
+        stock_30_vols = stock_30.reshape(-1)
 
-    right_pipette.pick_up_tip()
+        stock_30_aspirate_height = 88.5
 
-    for well, dispense_vol in zip(reagent_2ml_eppendorfs.wells(), stock_30_vols):
+        right_pipette.pick_up_tip()
 
-        distribute_stock_30(well, stock_30_aspirate_height, dispense_vol)
-        stock_30_aspirate_height = stock_30_aspirate_height + 0.2
+        for well, dispense_vol in zip(reagent_2ml_eppendorfs.wells(), stock_30_vols):
 
-    right_pipette.drop_tip()
+            distribute_stock_30(well, stock_30_aspirate_height, dispense_vol)
+            stock_30_aspirate_height = stock_30_aspirate_height + 0.2
 
-    ###################################### make 50x and mix
+        right_pipette.drop_tip()
 
-    sample_50x_aspirate_height = 35
+        ###################################### make 20x and mix
 
-    right_pipette.transfer(50,
-                            BSA_standards_2ml_eppendorfs.wells_by_name()['A6'].top(-sample_50x_aspirate_height),
+        sample_20x_aspirate_height = 38
+
+        right_pipette.transfer(80,
+                                BSA_standards_2ml_eppendorfs.wells_by_name()['A6'].top(-sample_20x_aspirate_height),
+                                reagent_2ml_eppendorfs.wells_by_name()['A1'].top(-sample_20x_aspirate_height),
+                                new_tip='always',
+                                # mix 2 times with 50uL before aspirating
+                                mix_before=(5, 60),
+                                mix_after=(10,120)
+                                )
+
+
+        ################################## make x40 and mix
+
+        sample_50x_aspirate_height = 38
+
+        right_pipette.transfer(50,
                             reagent_2ml_eppendorfs.wells_by_name()['A1'].top(-sample_50x_aspirate_height),
+                            reagent_2ml_eppendorfs.wells_by_name()['B1'].top(-sample_50x_aspirate_height),
                             new_tip='always',
                             # mix 2 times with 50uL before aspirating
-                            mix_before=(5, 60),
-                            mix_after=(10,200)
+                            mix_before=(3, 100),
+                            mix_after=(10,60)
                             )
 
+        ################################## make x100 and mix
 
-    ################################## make x100 and mix
+        sample_50x_aspirate_height = 38
 
-    sample_50x_aspirate_height = 35
+        right_pipette.transfer(20,
+                            reagent_2ml_eppendorfs.wells_by_name()['A1'].top(-sample_50x_aspirate_height),
+                            reagent_2ml_eppendorfs.wells_by_name()['C1'].top(-sample_50x_aspirate_height),
+                            new_tip='always',
+                            # mix 2 times with 50uL before aspirating
+                            mix_before=(3, 80),
+                            mix_after=(10,60)
+                            )
+        ################################## make x250 and mix
 
-    right_pipette.transfer(120,
-                        reagent_2ml_eppendorfs.wells_by_name()['A1'].top(-sample_50x_aspirate_height),
-                        reagent_2ml_eppendorfs.wells_by_name()['B1'].top(-sample_50x_aspirate_height),
-                        new_tip='always',
-                        # mix 2 times with 50uL before aspirating
-                        mix_before=(3, 100),
-                        mix_after=(10,190)
-                        )
+        #sample_50x_aspirate_height = 35
 
-    ################################## make x250 and mix
+        #right_pipette.transfer(50,
+        #                    reagent_2ml_eppendorfs.wells_by_name()['A1'].top(-sample_50x_aspirate_height),
+        #                    reagent_2ml_eppendorfs.wells_by_name()['C1'].top(-sample_50x_aspirate_height),
+        #                    new_tip='always',
+        #                    # mix 2 times with 50uL before aspirating
+        #                    mix_before=(3, 50),
+        #                    mix_after=(10,220)
+        #                    )
 
-    sample_50x_aspirate_height = 35
+        ################################## make x400 and mix
+        #sample_50x_aspirate_height = 35
+        #right_pipette.transfer(50,
+        #                    reagent_2ml_eppendorfs.wells_by_name()['B1'].top(-sample_50x_aspirate_height),
+        #                    reagent_2ml_eppendorfs.wells_by_name()['D1'].top(-sample_50x_aspirate_height),
+        #                    new_tip='always',
+        #                    # mix 2 times with 50uL before aspirating
+        #                    mix_before=(3, 50),
+        #                    mix_after=(10,170)
+        #                    )
 
-    right_pipette.transfer(50,
-                        reagent_2ml_eppendorfs.wells_by_name()['A1'].top(-sample_50x_aspirate_height),
-                        reagent_2ml_eppendorfs.wells_by_name()['C1'].top(-sample_50x_aspirate_height),
-                        new_tip='always',
-                        # mix 2 times with 50uL before aspirating
-                        mix_before=(3, 50),
-                        mix_after=(10,220)
-                        )
-
-    ################################## make x400 and mix
-    sample_50x_aspirate_height = 35
-    right_pipette.transfer(50,
-                        reagent_2ml_eppendorfs.wells_by_name()['B1'].top(-sample_50x_aspirate_height),
-                        reagent_2ml_eppendorfs.wells_by_name()['D1'].top(-sample_50x_aspirate_height),
-                        new_tip='always',
-                        # mix 2 times with 50uL before aspirating
-                        mix_before=(3, 50),
-                        mix_after=(10,170)
-                        )
-
-    ################################## make x500 and mix
-    sample_50x_aspirate_height = 35
-    right_pipette.transfer(30,
-                        reagent_2ml_eppendorfs.wells_by_name()['B1'].top(-sample_50x_aspirate_height),
-                        reagent_2ml_eppendorfs.wells_by_name()['A2'].top(-sample_50x_aspirate_height),
-                        new_tip='always',
-                        # mix 2 times with 50uL before aspirating
-                        mix_before=(3, 50),
-                        mix_after=(10,130)
-                        )
+        ################################## make x500 and mix
+        #sample_50x_aspirate_height = 35
+        #right_pipette.transfer(30,
+        #                    reagent_2ml_eppendorfs.wells_by_name()['B1'].top(-sample_50x_aspirate_height),
+        #                    reagent_2ml_eppendorfs.wells_by_name()['A2'].top(-sample_50x_aspirate_height),
+        #                    new_tip='always',
+        #                    # mix 2 times with 50uL before aspirating
+        #                    mix_before=(3, 50),
+        #                    mix_after=(10,130)
+        #                    )
 
 
-    ################################## make x600 and mix
-    sample_50x_aspirate_height = 35
-    right_pipette.transfer(30,
-                        reagent_2ml_eppendorfs.wells_by_name()['B1'].top(-sample_50x_aspirate_height),
-                        reagent_2ml_eppendorfs.wells_by_name()['B2'].top(-sample_50x_aspirate_height),
-                        new_tip='always',
-                        # mix 2 times with 50uL before aspirating
-                        mix_before=(3, 50),
-                        mix_after=(10,140)
-                        )
+        ################################## make x600 and mix
+        #sample_50x_aspirate_height = 35
+        #right_pipette.transfer(30,
+        #                    reagent_2ml_eppendorfs.wells_by_name()['B1'].top(-sample_50x_aspirate_height),
+        #                    reagent_2ml_eppendorfs.wells_by_name()['B2'].top(-sample_50x_aspirate_height),
+        #                    new_tip='always',
+        #                    # mix 2 times with 50uL before aspirating
+        #                    mix_before=(3, 50),
+        #                    mix_after=(10,140)
+        #                    )
 
-    ################################## make x750 and mix
-    sample_50x_aspirate_height = 35
-    right_pipette.transfer(50,
-                        reagent_2ml_eppendorfs.wells_by_name()['C1'].top(-sample_50x_aspirate_height),
-                        reagent_2ml_eppendorfs.wells_by_name()['C2'].top(-sample_50x_aspirate_height),
-                        new_tip='always',
-                        # mix 2 times with 50uL before aspirating
-                        mix_before=(3, 50),
-                        mix_after=(10,130)
-                        )
+        ################################## make x750 and mix
+        #sample_50x_aspirate_height = 35
+        #right_pipette.transfer(50,
+        #                    reagent_2ml_eppendorfs.wells_by_name()['C1'].top(-sample_50x_aspirate_height),
+        #                    reagent_2ml_eppendorfs.wells_by_name()['C2'].top(-sample_50x_aspirate_height),
+        #                    new_tip='always',
+        #                    # mix 2 times with 50uL before aspirating
+        #                    mix_before=(3, 50),
+        #                    mix_after=(10,130)
+        #                    )
 
 
-    ################################## make x1000 and mix
-    sample_50x_aspirate_height = 35
-    right_pipette.transfer(30,
-                        reagent_2ml_eppendorfs.wells_by_name()['B2'].top(-sample_50x_aspirate_height),
-                        reagent_2ml_eppendorfs.wells_by_name()['D2'].top(-sample_50x_aspirate_height),
-                        new_tip='always',
-                        # mix 2 times with 50uL before aspirating
-                        mix_before=(3, 50),
-                        mix_after=(8,130)
-                        )
+        ################################## make x1000 and mix
+        #sample_50x_aspirate_height = 35
+        #right_pipette.transfer(30,
+        #                    reagent_2ml_eppendorfs.wells_by_name()['B2'].top(-sample_50x_aspirate_height),
+        #                    reagent_2ml_eppendorfs.wells_by_name()['D2'].top(-sample_50x_aspirate_height),
+        #                    new_tip='always',
+        #                    # mix 2 times with 50uL before aspirating
+        #                    mix_before=(3, 50),
+        #                    mix_after=(8,130)
+        #                    )
 
 
     ################################## plate Bradford reagent
 
-    bradford_arr = np.array([
-                            [300, 300, 300, 300, 300, 300, 300, 300],
-                            [300, 300, 300, 300, 300, 300, 300, 300],
-                            [300, 300, 300, 300, 300, 300, 300, 300],
+    if bradford_toggle:
 
-                            [0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0],
+        bradford_arr = np.array([
+                                [300, 300, 300, 0, 0, 0, 0, 0],
+                                [300, 300, 300, 0, 0, 0, 0, 0],
+                                [300, 300, 300, 0, 0, 0, 0, 0],
 
-                            [0, 0, 0, 0, 0, 0, 0, 0],
-                            [300, 300, 300, 300, 300, 300, 300, 300],
-                            [300, 300, 300, 300, 300, 300, 300, 300],
-                            [300, 300, 300, 300, 300, 300, 300, 300],
-                            [300, 300, 300, 300, 300, 300, 300, 300],
-                            [300, 300, 300, 300, 300, 300, 300, 300]
-                            ])
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
 
-    # flatten array
-    bradford_arr = bradford_arr.reshape(-1)
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0]
 
-    # initial aspiriation height for initial volume of 22.5ml of bradford
-    bradford_aspirate_height = 62
+                                #[300, 300, 300, 300, 300, 300, 300, 300],
+                                #[300, 300, 300, 300, 300, 300, 300, 300],
+                                #[300, 300, 300, 300, 300, 300, 300, 300],
+                                #[300, 300, 300, 300, 300, 300, 300, 300],
+                                #[300, 300, 300, 300, 300, 300, 300, 300]
+                                ])
 
-    right_pipette.pick_up_tip()
+        # flatten array
+        bradford_arr = bradford_arr.reshape(-1)
 
-    for well, dispense_vol in zip(plate.wells(), bradford_arr):
+        # initial aspiriation height for initial volume of 22.5ml of bradford
+        bradford_aspirate_height = 62
 
-        distribute_bradford(well, bradford_aspirate_height, dispense_vol)
+        right_pipette.pick_up_tip()
 
-        #### unsure about correct descent speed.
-        bradford_aspirate_height = bradford_aspirate_height + 0.5
+        for well, dispense_vol in zip(plate.wells(), bradford_arr):
 
-    right_pipette.drop_tip()
+            distribute_bradford(well, bradford_aspirate_height, dispense_vol)
+
+            #### unsure about correct descent speed.
+            bradford_aspirate_height = bradford_aspirate_height + 0.5
+
+        right_pipette.drop_tip()
 
 
 
     ################################## plate standards
 
-    asp_wells = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2', 'C2', 'D2']
-    disp_rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    if plate_standards_toggle:
 
-    disp_vols = [0, 0, 0, 0, 0, 0, 0, 10, 10, 10, 10, 10]
+        asp_wells = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2', 'C2', 'D2']
+        disp_rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
-    aspirate_plate = BSA_standards_2ml_eppendorfs
+        disp_vols = [0, 0, 0, 0, 0, 0, 0, 10, 10, 10, 10, 10]
 
-
-
-    for aspirate_well, dispense_row in zip(asp_wells, disp_rows):
-
-        for dispense_well, dispense_volume in zip(plate.rows_by_name()[dispense_row], disp_vols):
-
-            distribute_protein_mix(aspirate_well, aspirate_plate, dispense_well, dispense_volume)
+        aspirate_plate = BSA_standards_2ml_eppendorfs
 
 
-    #############################################################################
+
+        for aspirate_well, dispense_row in zip(asp_wells, disp_rows):
+
+            for dispense_well, dispense_volume in zip(plate.rows_by_name()[dispense_row], disp_vols):
+
+                distribute_standards_mix(aspirate_well, aspirate_plate, dispense_well, dispense_volume)
+
+
+        #############################################################################
 
     ################################## plate dilutions #1
 
-    asp_wells = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2', 'C2', 'D2']
-    disp_rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    if plate_sample_dilutions:
 
-    disp_vols = [10, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        asp_wells = ['A1', 'B1', 'C1']
+        disp_rows = ['A', 'B', 'C']
 
-    aspirate_plate = reagent_2ml_eppendorfs
+        disp_vols = [10, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    for aspirate_well, dispense_row in zip(asp_wells, disp_rows):
+        aspirate_plate = reagent_2ml_eppendorfs
 
-        for dispense_well, dispense_volume in zip(plate.rows_by_name()[dispense_row], disp_vols):
+        for aspirate_well, dispense_row in zip(asp_wells, disp_rows):
 
-            distribute_protein_mix(aspirate_well, aspirate_plate, dispense_well, dispense_volume)
+            for dispense_well, dispense_volume in zip(plate.rows_by_name()[dispense_row], disp_vols):
+
+                distribute_protein_mix(aspirate_well, aspirate_plate, dispense_well, dispense_volume)
