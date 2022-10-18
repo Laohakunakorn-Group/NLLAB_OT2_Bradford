1e54e264956058a1d5cfb2d6a72908faa40b3635
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2022-10-18 11:57:36 +0100

    updated to be more flexible to different plate reader exports, included parser for biotekh1

--- analysis/bayesian_bradford_data_analysis_package/experiment_specific_config.py
+++ analysis/bayesian_bradford_data_analysis_package/experiment_specific_config.py
@@ -1,8 +1,24 @@
 
-num_calibrant_replicates = 5
+unknown_proteins = {'OPP'}
 
-num_experiment_replicates = 3
+dilutions_dict = {
+	"OPP": {
+	"20":["A1", "A2", "A3"],
+	"40":["B1", "B2", "B3"],
+	"60": ["C1", "C2", "C3"]
+		}
+}
 
-unknown_proteins = ['AA tRNA Synthetases']
+calibrants_dict = {
+	"0": ["A8", "A9", "A10", "A11", "A12"],
+	"50": ["B8", "B9", "B10", "B11", "B12"],
+	"75": ["C8", "C9", "C10", "C11", "C12"],
+	"100": ["D8", "D9", "D10", "D11", "D12"],
+	"125": ["E8", "E9", "E10", "E11", "E12"],
+	"150": ["F8", "F9", "F10", "F11", "F12"],
+	"175": ["G8", "G9", "G10", "G11", "G12"],
+	"200": ["H8", "H9", "H10", "H11", "H12"]
+}
 
-dilutions = ['50', '100', '250', '400', '500', '600', '750', '1000']
+
+plate_reader = "Biotek H1"
