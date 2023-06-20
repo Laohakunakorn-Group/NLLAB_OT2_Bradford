7bcf1e1c0a1e55bb28dccf44e4f6a91bf2ef0ff4
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-06-21 00:04:53 +0100

    updated cali data set with a mu

--- streamlit/datasets/calibration_wrangle.py
+++ streamlit/datasets/calibration_wrangle.py
@@ -5,6 +5,8 @@ raw = pd.read_csv("calibration.csv")
 
 print(raw)
 
+#raw = raw.rename(columns={'Bradford_Volume_ul': 'Bradford_Volume_µl', 'Sample_Volume_ul': 'Sample_Volume_µl'})
+#print(raw)
 #raw = raw.drop("Unnamed: 0", axis = 1)
 
 raw.to_csv("calibration.csv", index=None)
